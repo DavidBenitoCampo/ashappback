d0d4284e764661273c701c46ebf7e37a976fc5b9
Cristian Gomez <crisstianc@gmail.com>
2021-03-23 17:26:39 +0100

    Revert "x"
    This reverts commit 2481d04b383ce1a6038cc8a2b2ff24e5545788cb.

--- routes/middleware.js
+++ routes/middleware.js
@@ -8,12 +8,13 @@ const checkToken = (req, res, next) => {
     //comprobar si el token es valido.
 
     const token = req.headers['authorization']
-    let data
     try {
         const data = jwt.verify(token, 'lolita');
     } catch (error) {
         return res.json({ error: 'el token es incorrecto' })
     }
+
+
     //comprobar si el token está caducado.
     next();
 }
